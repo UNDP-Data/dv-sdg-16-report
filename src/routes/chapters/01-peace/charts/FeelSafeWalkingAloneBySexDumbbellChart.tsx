177e3691217fae334795886113be420d6adcb2a6
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2026-08-14 16:42:11 +0300

    Create dumbbell chart about feeling safe at night

---
+++ src/routes/chapters/01-peace/charts/FeelSafeWalkingAloneBySexDumbbellChart.tsx
@@ -0,0 +1,38 @@
+import { Colors } from '@undp/data-viz/Colors';
+import { DumbbellChart } from '@undp/data-viz/DumbbellChart';
+import { transformDataForGraph } from '@undp/data-viz/transformData';
+import { P } from '@undp/design-system-react/Typography';
+import { CHART_PADDING } from '@/constants';
+import feelSafeWalkingAloneByRegionSex from '@/data/chapters/01-peace/16-1-4/feel-safe-walking-alone-by-region-sex.json';
+import ChartNote from '../../components/ChartNote';
+
+export default function FeelSafeWalkingAloneBySexDumbbellChart() {
+  return (
+    <DumbbellChart
+      data={transformDataForGraph(feelSafeWalkingAloneByRegionSex, 'dumbbellChart', [
+        { columnId: 'region', chartConfigId: 'label' },
+        { columnId: ['female', 'male'], chartConfigId: 'x' },
+      ])}
+      orientation='horizontal'
+      colorDomain={['Women', 'Men']}
+      colors={[Colors.genderColors.female, Colors.genderColors.male]}
+      minValue={0}
+      showTicks={false}
+      maxValue={100}
+      numberDisplayOptions={{ suffix: '%' }}
+      padding={CHART_PADDING}
+      backgroundColor='var(--background-soft)'
+      graphTitle={
+        <P marginBottom='none' className='font-heading font-semibold leading-sm'>
+          Proportion of the population that feel safe walking alone at night around the area they
+          live in, by region and sex (2021–2025)
+        </P>
+      }
+      sources={[{ source: 'UNODC' }]}
+      footNote={
+        <ChartNote content='Estimates are based on responses to the United Nations Survey of Crime Trends and Operations of Criminal Justice Systems, Multiple Indicator Cluster Surveys, and the Gallup World Poll. Regional aggregates refer to 3-year averages weighted by countries’ population size. Most surveys include the qualifications “after dark” or “at night” in the question wording. Averages for Oceania were removed, as only data for one country was available.' />
+      }
+      ariaLabel='Dumbbell chart showing the share of the population that feels safe walking alone at night, by region and sex, from 2021 to 2025. Women report feeling less safe than men in every region, with the widest gap in Northern Africa and Western Asia.'
+    />
+  );
+}
