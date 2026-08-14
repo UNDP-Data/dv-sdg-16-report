314866b779966fe70f22f202c87e7dc5c41585ca
Mustafa Saifee <saifee.mustafa@gmail.com>
2026-08-14 10:55:05 +0300

    Update WaveDivider.tsx

--- src/routes/chapters/components/WaveDivider.tsx
+++ src/routes/chapters/components/WaveDivider.tsx
@@ -1,18 +1,14 @@
-import { cn } from '@undp/design-system-react/cn';
-
-interface WaveDividerProps {
+export default function WaveDivider({
+  src,
+  align = 'left',
+}: {
   src: string;
   align?: 'left' | 'right';
-}
-
-export default function WaveDivider({ src, align = 'left' }: WaveDividerProps) {
+}) {
   return (
     <div
       aria-hidden='true'
-      className={cn(
-        'relative left-1/2 flex h-55 w-screen -translate-x-1/2 overflow-hidden',
-        align === 'right' ? 'justify-end' : 'justify-start',
-      )}
+      className={`relative left-1/2 flex h-55 w-screen -translate-x-1/2 overflow-hidden ${align === 'right' ? 'justify-end' : 'justify-start'}`}
     >
       <img
         src={src}
