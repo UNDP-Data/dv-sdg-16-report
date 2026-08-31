8fa0d5d3787911db14b5c3ebb2def719225dfc59
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2026-08-31 22:30:38 +0300

    Clean constants file

--- src/constants.ts
+++ src/constants.ts
@@ -6,36 +6,3 @@ export const CHART_HIGHLIGHT_AREA_BG = '#E6F0F7';
 export const CHART_HIGHLIGHT_LABEL_COLOR = '#36566f';
 
 export const PROJECTED_PERIOD = [2025, 2030];
-
-export const SUBSECTIONS_BY_CHAPTER = {
-  peace: [
-    'Conflict-related deaths',
-    'Homicide',
-    'Attacks on defenders',
-    'Physical, sexual and psychological violence',
-    'Violence against children',
-    'Trafficking in persons',
-    'Perception of safety',
-  ],
-  justice: [
-    'Access to criminal justice',
-    'Access to civil justice',
-    'Unsentenced detention',
-    'Illicit financial flows',
-    'Illicit firearms flows',
-    'Bribery among the population',
-    'Bribery of businesses',
-    'National human rights institutions',
-  ],
-  inclusion: [
-    'Legal identity',
-    'Representation in the legislature',
-    'Representation in public service institutions and the judiciary',
-    'Experience of discrimination',
-    'Government expenditures',
-    'Satisfaction with public services',
-    'Political voice and responsiveness',
-    'Access to information',
-    'Representation of developing countries in international organizations',
-  ],
-};
