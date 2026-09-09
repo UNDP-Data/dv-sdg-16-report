27d810579dece80c15c14e6051ed08c66bd267b5
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2026-09-09 11:33:28 +0300

    Update 'access to information' big numbers and notes

--- src/routes/report/04-inclusion/charts/AccessToInformationDisclosureBigNumbers.tsx
+++ src/routes/report/04-inclusion/charts/AccessToInformationDisclosureBigNumbers.tsx
@@ -1,6 +1,7 @@
 import { P } from '@undp/design-system-react/Typography';
 import BigNumber from '@/components/BigNumber';
 import { CHART_PADDING } from '@/constants';
+import ChartNote from '../../components/ChartNote';
 
 export default function AccessToInformationDisclosureBigNumbers() {
   return (
@@ -15,23 +16,35 @@ export default function AccessToInformationDisclosureBigNumbers() {
       </div>
 
       <div className='grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-3 md:gap-x-10'>
-        <BigNumber color='blue-500' value={38} suffix='%' label='no substantive response' />
-        <BigNumber color='blue-500' value={54} suffix='%' label='at least some information' />
+        <BigNumber
+          color='blue-500'
+          value={38}
+          suffix='%'
+          label='mute refusals / no substantive response'
+        />
         <BigNumber color='blue-500' value={42} suffix='%' label='full disclosure' />
+        <BigNumber
+          color='blue-500'
+          value={54}
+          suffix='%'
+          label='full disclosure or at least some information provided'
+        />
       </div>
-
-      <P marginBottom='none' size='sm' className='text-content-secondary'>
-        Source: Mendel, T. and Vagliano, R. (2025) Global Comparative Testing of Responses to
-        Requests for Information. Halifax, NS: Centre for Law and Democracy. Available at:{' '}
-        <a
-          href='https://www.law-democracy.org/wp-content/uploads/2026/02/IDUAI.report.25-09-26.pdf'
-          target='_blank'
-          className='underline'
-          rel='noopener noreferrer'
-        >
-          https://www.law-democracy.org/wp-content/uploads/2026/02/IDUAI.report.25-09-26.pdf
-        </a>
-      </P>
+      <div>
+        <P marginBottom='none' size='sm' className='text-content-secondary'>
+          Source:{' '}
+          <a
+            href='https://www.law-democracy.org/wp-content/uploads/2026/02/IDUAI.report.25-09-26.pdf'
+            target='_blank'
+            className='underline'
+            rel='noopener noreferrer'
+          >
+            Mendel, T. and Vagliano, R. (2025) Global Comparative Testing of Responses to Requests
+            for Information. Halifax, NS: Centre for Law and Democracy.
+          </a>
+        </P>
+        <ChartNote content='The remaining 8% of outcomes comprised cases where an information request could not be submitted (5 cases), requests that were denied (3 cases), and other outcomes that did not fall within the categories described above (4 cases). ' />
+      </div>
     </div>
   );
 }
