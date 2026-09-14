2a24d20c6a3076a16b630f5417d4cda7d2cdbadc
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2026-09-14 10:50:56 +0300

    Fix padding so that text fits in the boxes

--- src/routes/report/components/HeroBanner.tsx
+++ src/routes/report/components/HeroBanner.tsx
@@ -46,7 +46,7 @@ export default function HeroBanner({
       className='relative h-auto bg-center bg-cover bg-foreground-soft md:min-h-[calc(100vh-102px)]'
       style={{ backgroundImage: `url('${bg}')` }}
     >
-      <div className='mx-auto flex w-full max-w-300 flex-col items-start gap-4 px-6 py-20 md:px-12 md:py-28'>
+      <div className='mx-auto flex w-full max-w-300 flex-col items-start gap-4 px-6 py-20 md:px-11 md:py-28'>
         <div className='flex items-center gap-3 text-content-reverse'>
           <P
             marginBottom='none'
