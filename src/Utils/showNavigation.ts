5635ce1220c46a4cc646ca94561f90082cb06234
Mustafa Saifee <saifee.mustafa@gmail.com>
2026-09-23 20:07:22 +0300

    Update showNavigation.ts

--- src/Utils/showNavigation.ts
+++ src/Utils/showNavigation.ts
@@ -3,5 +3,5 @@ import { useLocation } from '@tanstack/react-router';
 export const showNavigation = () => {
   const { pathname, search } = useLocation();
 
-  return pathname !== '/' || new Date() > new Date('2026-09-23T21:00:00Z') || search.bypassCounter;
+  return pathname !== '/' || new Date() > new Date('2026-09-23T20:50:00Z') || search.bypassCounter;
 };
